b72380cf6cc40844f2dc8023b6b89e4f01b81a1b
SirPoire <mipoire@hotmail.com>
2023-09-15 15:09:44 -0400

    update scaling for new loops adding + stop button

--- code/interface.py
+++ code/interface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import sv_ttk
 from .player import Player
+import os
 
 
 class Window(tk.Tk):
@@ -8,13 +9,14 @@ class Window(tk.Tk):
         super().__init__()
         self.title("Beat machine")
         sv_ttk.use_light_theme()
-        self.geometry("400x200")
+        self.geometry("800x600")
 
         self.button_width = 10
-        self.button_height = 5
+        self.button_height = 3
         self.police = ('calibre', 14, 'bold')
 
         self.player = Player()
+        self.beats = self.player.load_beats()
 
         self.volume_fader = None
         self.volume = tk.DoubleVar()
@@ -22,27 +24,41 @@ class Window(tk.Tk):
         self.create_widget()
 
     def create_widget(self):
-        lofi_button = tk.Button(self,
-                                text="LoFi",
+        row_position = 1
+        for category, beat_list in self.beats.items():
+            category_label = tk.Label(self, text=category, font=self.police)
+            category_label.grid(row=row_position, column=1, sticky='w')
+            num = 0
+            for idx, beat in enumerate(beat_list):
+                beat_name = os.path.splitext(self.player.file_dict[category][num])[0]
+                beat_button = tk.Button(self,
+                                        text=beat_name,
+                                        width=self.button_width,
+                                        height=self.button_height,
+                                        command=lambda b=beat: self.toggle_beat(b),
+                                        font=self.police)
+                beat_button.grid(row=row_position, column=idx + 2, sticky='w')
+                num += 1
+
+            row_position += 1
+
+        self.volume_fader = tk.Scale(self,
+                                     from_=100,
+                                     to=0,
+                                     orient="vertical",
+                                     variable=self.volume,
+                                     command=self.update_volume)
+        self.volume_fader.set(100)
+        self.volume_fader.grid(row=1, column=20, rowspan=10)
+        self.update_volume()
+
+        stop_button = tk.Button(self,
+                                text="Stop",
                                 width=self.button_width,
                                 height=self.button_height,
-                                command=lambda: self.toggle_beat(self.player.lofi),
-                                font=self.police
-                                )
-        lofi_button.grid(row=10, column=10)
-
-        groovy_button = tk.Button(self,
-                                  text="Groovy",
-                                  width=self.button_width,
-                                  height=self.button_height,
-                                  command=lambda: self.toggle_beat(self.player.groovy),
-                                  font=self.police)
-        groovy_button.grid(row=10, column=20)
-
-        self.volume_fader = tk.Scale(self, from_=100, to=0, orient="vertical", variable=self.volume, command=self.update_volume)
-        self.volume_fader.set(100)
-        self.volume_fader.grid(row=10, column=100, columnspan=100)
-        self.update_volume()
+                                command=self.stop_all_beats,
+                                font=self.police)
+        stop_button.grid(row=1, column=30, rowspan=10)
 
     def toggle_beat(self, beat):
         if self.player.current_beat != beat:
@@ -56,3 +72,8 @@ class Window(tk.Tk):
         if self.player.current_beat is not None:
             volume = self.volume.get() / 100.0
             self.player.current_beat.set_volume(volume)
+
+    def stop_all_beats(self):
+        if self.player.current_beat:
+            self.player.stop(self.player.current_beat)
+            self.player.current_beat = None
--- code/player.py
+++ code/player.py
@@ -1,15 +1,13 @@
 import pygame
+import os
 
 
 class Player():
     def __init__(self):
         pygame.init()
         pygame.mixer.init()
-
-        self.lofi = pygame.mixer.Sound("AUDIO/LoFi.wav")
-        self.groovy = pygame.mixer.Sound("AUDIO/Groovy-96bpm.wav")
-
         self.current_beat = None
+        self.file_dict = {}
 
     def play(self, beat, callback=None):
         self.stop(self.current_beat)
@@ -19,4 +17,19 @@ class Player():
     def stop(self, beat):
         if beat is not None:
             beat.stop()
+
+    def load_beats(self):
+        beat_folder = "audio"
+        beats = {}
+        for category in os.listdir(beat_folder):
+            category_path = os.path.join(beat_folder, category)
+            if os.path.isdir(category_path):
+                self.file_dict[category] = os.listdir(category_path)
+                beats[category] = []
+                for filename in os.listdir(category_path):
+                    if filename.endswith(".wav"):
+                        beat_path = os.path.join(category_path, filename)
+                        beat = pygame.mixer.Sound(beat_path)
+                        beats[category].append(beat)
+        return beats
 
